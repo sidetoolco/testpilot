0f1b6029adcc919fec5ee306b8fb5708145166d6
Felipe Buscaglia <felipe.buscaglia@sidetool.co>
2025-06-02 09:55:01 -0300

    Only initialize Sentry on production environment

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,15 @@ import { browserTracingIntegration } from '@sentry/browser';
 import App from './App';
 import './index.css';
 
-Sentry.init({
-  dsn: "https://765ff27999bc8f1f5fb1e9d570aa7b2a@o4509390953316352.ingest.us.sentry.io/4509390963277824",
-  integrations: [browserTracingIntegration()],
-  tracesSampleRate: 1.0,
-  environment: import.meta.env.MODE,
-  sendDefaultPii: true
-});
+if (import.meta.env.PROD) {
+  Sentry.init({
+    dsn: "https://765ff27999bc8f1f5fb1e9d570aa7b2a@o4509390953316352.ingest.us.sentry.io/4509390963277824",
+    integrations: [browserTracingIntegration()],
+    tracesSampleRate: 1.0,
+    environment: import.meta.env.MODE,
+    sendDefaultPii: true
+  });
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
